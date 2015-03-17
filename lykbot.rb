1f57edd1bfb2a3167fa6802ec702a3a765e096ab
lykranian <supermanerik@gmail.com>
2015-03-17 08:41:26 -0700

    basically it was broken (i didn't test it)

--- lykbot.rb
+++ lykbot.rb
@@ -16,11 +16,10 @@ bot = Cinch::Bot.new do
     c.realname = "lykbot"
   end
 
-version = "1.0.0"
+version = "1.0.1"
 
 #set which channels get rss udates
-$rss_chans = ["#channel1",
-              "#channel2"]
+$rss_chans = ["#channel1"]
 
 #set a nick as an admin
 $admin = "lykranian"
@@ -37,7 +36,7 @@ $admin = "lykranian"
 
   #.join #chan
   on :message, /^\.join (.+)/ do |m, channel|
-    if m.user.nick == admin
+    if m.user.nick == $admin
       Channel(channel).join
     end
   end
@@ -54,30 +53,32 @@ $admin = "lykranian"
     m.reply "latest release - #{rss.items.first.title}"
   end
 
-
   #RSS auto updating
-
   $rss_state = "on"
-  on :message, ".rss start" do |m|
-    if m.user.nick == admin
+  on :message, ".rssstart" do |m|
+    if m.user.nick == $admin
       #loads feed links from a .yml file
       $feed_link = YAML.load File.open('feed_link.yml', 'r')
       length = $feed_link.length - 1
       $old = Array.new($feed_link.length, "empty")
       #gets the current most recent articles for each feed link
       for i in 0..length
-        $old[i] = get_first_title($feed_link[i])
+        oldfeed = SimpleRSS.parse(open($feed_link[i]))
+        $old[i] = oldfeed.items.first.title
         sleep(1)
       end
       while $rss_state == "on"
         for i in 0..length
-          #to do: make it retry a limited number of times,
-          #to prevent infinite loop if feed server is down
+            times = 0
           begin
             #gets new feed for each link
             feed = SimpleRSS.parse(open($feed_link[i]))
           rescue Exception
-            retry
+            #should only attemt a retry 3 times
+            if times < 3
+              retry
+              times = times + 1
+            end
           end
           sleep(1)
           #compares the stored value with the new feed
@@ -101,8 +102,8 @@ $admin = "lykranian"
 
   #might refresh the rss feed list without having to restart the bot
   #need to add a check to make sure it doesn't do anything while updates are being checked for above
-  on :message, ".rss load" do |m|
-    if m.user.nick == admin
+  on :message, ".rssload" do |m|
+    if m.user.nick == $admin
       $feed_link = YAML.load File.open('feed_link.yml', 'r')
       length = $feed_link.length - 1
       $old = Array.new($feed_link.length, "empty")
@@ -132,7 +133,7 @@ $admin = "lykranian"
   $recipient = YAML.load File.open('recipient.yml', 'r')
   $message = YAML.load File.open('message.yml', 'r')
   $time_send = YAML.load File.open('time_send.yml', 'r')
-  #mail send count
+  #mail send count, keeps track of where to put and load from in arrays
   $msc = $sender.length
 
   on :message, ".mail" do |m|
@@ -144,11 +145,12 @@ $admin = "lykranian"
     $sender[$msc] = m.user.nick
     $recipient[$msc] = recipient_down
     $message[$msc] = message_get
+    #this stores the time of sending in unix time, because timezones are hard to think about
     time_temp = Time.now.to_i
     $time_send[$msc] = time_temp
     User(m.user.nick).send "your mail is on its way to #{recipient_get}'s mailbox"
     $msc = $msc + 1
-    #writes mail info so there's an up-to-date version if the bot shuts downn
+    #writes mail info so there's an up-to-date version if the bot shuts down
     #prevents messages from getting lost
     File.open('sender.yml', 'w') do |f|
       f.write $sender.to_yaml
@@ -170,7 +172,7 @@ $admin = "lykranian"
   end
 
   #tells people if they have mail if they join a channel with the bot
-  on :join, do |m|
+  on :join do |m|
     length = $recipient.length - 1
     count = 0
     for i in 0..length
@@ -184,20 +186,23 @@ $admin = "lykranian"
     end
   end
 
-#checks and sends mail for each message that the bot recieves
-  on :message, do |m|
+  #checks and sends mail for _every_ message that the bot recieves
+  #potentially fatal if joined to numerous busy channels
+  on :message do |m|
     length = $recipient.length - 1
     for i in 0..length
       rec_nick = $recipient [i]
       if m.user.nick.downcase == rec_nick.downcase
+        #finds the difference from send time and check time
         time_diff = Time.now.to_i - $time_send[i]
                      #vvvvvvvv this is where time_lord comes in
         time_check = time_diff.seconds.ago.to_words
-        User(m.user.nick).send "| from %s, #{time_check}: #{$message[i]}" % [Format(:yellow, "#{$sender[i]}")]
+        User(m.user.nick).send "| from %s, #{time_check}: #{$message[i]}" % [Format(:blue, "#{$sender[i]}")]
         #removes the recipient, so that the mail will never be sent again
         $recipient[i] = "~recieved~"
         #removes the message contents, so that pivacy is respected
-        $messagee[i] = "~revieved~"
+        $message[i] = "~revieved~"
+        #re-writes the mail arrays to the yaml files, with the replacement text in place
         #should probably write a function or something huh
         File.open('sender.yml', 'w') do |f|
           f.write $sender.to_yaml
